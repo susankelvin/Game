b74ab54b08bc39bfac54e3419784cafb32bbf56b
Dian <d.d.penchev@gmail.com>
2014-02-23 13:23:28 +0200

    Enemy added
    Sergeant

---
+++ BestGame/Enemies/Sergeant.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BestGame
+{
+    public class Sergeant : DestructableEnemy, IOffencible
+    {
+    }
+}
